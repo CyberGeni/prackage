8492e681a9f424b8ae1f1c87b969cd5e783bf6ff
Treasure Ajefu <treasureajefu@gmail.com>
2022-11-24 00:10:13 +0100

    structure of footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+        <section>
+            <p></p>
+            <button></button>
+        </section>
+        <section>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </section>
+        <section>
+            <h1></h1>
+            <span></span>
+        </section>
+    </footer>
+  )
+}
+
+export default Footer
