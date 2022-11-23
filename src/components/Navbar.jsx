191bd65472631d3fc9b85f92dec4724083ff7edd
Treasure Ajefu <treasureajefu@gmail.com>
2022-11-24 00:07:19 +0100

    structure for homepage and navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <h1></h1>
+        <div>
+            <nav>
+                <li></li>
+                <li></li>
+                <li></li>
+            </nav>
+            <div>
+                <button>Log In</button>
+                <button>Sign Up</button>
+            </div>
+        </div>
+        <img src="" alt="" />
+    </div>
+  )
+}
+
+export default Navbar
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <Navbar />
+        <main>
+            <section>
+                <h1>Build power, respect into your name.</h1>
+                <p>Prackage's innovative approach to personal branding ensures that your name shines in the right time.</p>
+                <button>Get started</button>
+            </section>
+            <section>
+                <span></span>
+                <div>
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </div>
+            </section>
+            <section>
+                <img src="" alt="" />
+                <div>
+                    <h1></h1>
+                    <p></p>
+                    <p></p>
+                </div>
+            </section>
+            <section>
+                <img src="" alt="" />
+                <div>
+                    <h1></h1>
+                    <p></p>
+                    <p></p>
+                </div>
+            </section>
+            <section>
+                <img src="" alt="" />
+                <div>
+                    <h1></h1>
+                    <p></p>
+                    <p></p>
+                </div>
+            </section>
+        </main>
+        <Footer />
+    </div>
   )
 }
 
