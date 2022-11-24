ee883e6a4e5fcde0da07a386a27c811db84bb086
Treasure Ajefu <treasureajefu@gmail.com>
2022-11-24 21:00:20 +0100

    finished styling for main sections with responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,9 @@ import TVC from '../assets/images/tvc-logo.svg'
 import Techcabal from '../assets/images/techcabal-logo.svg'
 import mtvbase from '../assets/images/mtvbase-logo.svg'
 import Pulse from '../assets/images/pulse-logo.svg'
+import SectionOneImg from '../assets/images/heavy-on-the-personal.svg'
+import SectionTwoImg from '../assets/images/made-with-diff-personalities-in-mind.svg'
+import SectionThreeImg from '../assets/images/meaningful-connections.svg'
 
 function Home() {
   return (
@@ -30,29 +33,29 @@ function Home() {
                     <img className="max-w-full" src={Pulse} alt="" />
                 </div>
             </section>
-            <section className="bg-blue-50">
-                <img src="" alt="" />
-                <div>
-                    <h1>Heavy on the “personal”</h1>
-                    <p>Simply put, we treat you like a person, a very important one.</p>
-                    <p>Prackage combines the scientific with a personal touch to drive all-round growth in you as a person because we care about how you appear outside as much as how you feel on the inside. Learn about our process</p>
-                    <button>Learn about our process</button>
+            <section className="bg-blue-50 p-12 sm:px-16 md:px-20 md:grid md:grid-cols-2 md:gap-10 md:items-center">
+                <img className="mx-auto md:w-full" src={SectionOneImg} alt="" />
+                <div className="order-first md:space-y-6">
+                    <h1 className="font-bold text-3xl md:text-4xl my-4">Heavy on the “personal”</h1>
+                    <p className="text-xl my-2 text-gray-600">Simply put, we treat you like a person, a very important one.</p>
+                    <p className="text-gray-500 text-lg">Prackage combines the scientific with a personal touch to drive all-round growth in you as a person because we care about how you appear outside as much as how you feel on the inside. Learn about our process</p>
+                    <button className="bg-blue-600 text-white rounded px-6 py-3 mt-6">Learn about our process</button>
                 </div>
             </section>
-            <section>
-                <img src="" alt="" />
-                <div>
-                    <h1>Made with different personalities in mind</h1>
-                    <p>With our proprietary tool Humark®, we are able to build and manage your personal brand to your taste, whether you love the spotlight or you prefer to stay BTS.</p>
-                    <button>Learn about Humark</button>
+            <section className="p-12 sm:px-16 md:px-20 md:grid md:grid-cols-2 md:gap-10 md:items-center">
+                <img className="rounded-md mx-auto md:w-full" src={SectionTwoImg} alt="" />
+                <div className="md:space-y-6">
+                    <h1 className="font-bold text-3xl md:text-4xl my-4">Made with different personalities in mind</h1>
+                    <p className="text-gray-500 text-lg my-3">With our proprietary tool Humark®, we are able to build and manage your personal brand to your taste, whether you love the spotlight or you prefer to stay BTS.</p>
+                    <button className="bg-blue-600 text-white rounded px-6 py-3">Learn about Humark</button>
                 </div>
             </section>
-            <section className="bg-orange-50">
-                <img src="" alt="" />
-                <div>
-                    <h1>Meaningful connections where and when it matters.</h1>
-                    <p>Prackage increases your presence and visibility in refreshing ways to make sure you remain top-of-mind in the minds of key people in your life.</p>
-                    <button>Get started</button>
+            <section className="bg-orange-50 p-12 sm:px-16 md:px-20 md:grid md:grid-cols-2 md:gap-10 md:items-center">
+                <img className="mx-auto md:w-full" src={SectionThreeImg} alt="" />
+                <div className="order-first md:space-y-6">
+                    <h1 className="font-bold text-3xl md:text-4xl my-4">Meaningful connections where and when it matters.</h1>
+                    <p className="text-gray-500 text-lg">Prackage increases your presence and visibility in refreshing ways to make sure you remain top-of-mind in the minds of key people in your life.</p>
+                    <button className="bg-blue-600 text-white rounded px-6 py-3 mt-4">Get started</button>
                 </div>
             </section>
         </main>
