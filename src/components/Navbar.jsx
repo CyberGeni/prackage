9869a0ca66985ac3c93693765c49221880abd547
Treasure Ajefu <treasureajefu@gmail.com>
2022-11-24 15:59:13 +0100

    feat: completed hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
+import Menu from '../assets/icons/menu.svg'
+import Close from '../assets/icons/close.svg'
 
 function Navbar() {
   return (
-    <div>
-        <h1></h1>
-        <div>
-            <nav>
-                <li></li>
-                <li></li>
-                <li></li>
-            </nav>
+    <header className="relative p-8 flex justify-between mx-auto list-none">
+        <h1 className="font-bold text-2xl">prackage</h1>
+        <div className="hidden flex flex-col justify-between w-full h-screen text-2xl space-y-4 absolute bg-white">
+          <div className="flex justify-between px-10 py-8">  
+            
+            <img 
+              className="w-4 cursor-pointer"
+              src={Close} alt="" />
+          </div>
+            
+          <nav className="space-y-5 px-10 h-full">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Pricing</li>
             <div>
-                <button>Log In</button>
-                <button>Sign Up</button>
+              <button>Log In</button>
             </div>
+          </nav>
+          <div className="flex flex-col">
+            <button className="text-xl w-full bg-blue-600 mb-0 text-white px-6 py-4">Sign Up</button>
+          </div>
         </div>
-        <img src="" alt="" />
-    </div>
+        <img src={Menu} alt="" />
+    </header>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,10 +8,10 @@ function Home() {
     <div className="font-BrOmega">
         <Navbar />
         <main>
-            <section>
-                <h1 className="">Build power, respect into your name.</h1>
-                <p>Prackage's innovative approach to personal branding ensures that your name shines in the right time.</p>
-                <button>Get started</button>
+            <section className="flex flex-col w-4/5 mx-auto justify-center items-center h-[80vh] space-y-5 sm:space-y-6 md:space-y-7">
+                <h1 className="text-5xl sm:text-6xl md:text-7xl md:max-w-2xl mx-auto text-center font-bold">Build power, respect into your <span className="decoration-underline underline underline-offset-4 decoration-blue-600">name.</span> </h1>
+                <p className="text-gray-500 text-center sm:text-lg md:text-xl md:max-w-xl">Prackage's innovative approach to personal branding ensures that your name shines in the right time.</p>
+                <button className="bg-blue-600 text-white rounded px-6 py-3">Get started</button>
             </section>
             <section>
                 <span></span>
