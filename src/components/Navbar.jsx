11c9eb762335a220e8fa445ba7e93a3df454fda8
Treasure Ajefu <treasureajefu@gmail.com>
2022-11-28 21:41:30 +0100

    fix: login button breaking on tablet view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ function Navbar() {
   }
 
   return (
-    <header className="relative flex justify-between mx-auto list-none">
+    <header className="relative flex justify-between mx-auto list-none overflow-hidden w-full">
         <h1 className="px-10 py-8 font-bold text-2xl">prackage</h1>
         <div className=
           {isActive ? "left-[0vw] md:left-unset transition-all flex flex-col md:flex-row justify-between w-full h-screen md:h-fit text-2xl md:text-base space-y-4 md:space-y-0 fixed md:relative bg-white" : "transition-all left-[100vw] absolute md:relative md:flex md:justify-between w-full md:left-0 md:text-lg md:my-2"}>
@@ -34,8 +34,8 @@ function Navbar() {
           </nav>
 
           <div className="flex flex-col w-auto md:space-x-3 md:pr-10 md:flex-row md:items-center md:justify-center space-y-4 md:space-y-0">
-            <button className="md:w-full">Log In</button>
-            <button className="text-xl md:text-base rounded w-full bg-blue-600 mb-0 text-white px-6 py-4 md:py-2 ">Sign Up</button>
+            <button className="md:w-max">Log In</button>
+            <button className="text-xl md:text-base rounded w-max bg-blue-600 mb-0 text-white px-6 py-4 md:py-2 ">Sign Up</button>
           </div>
         </div>
         <img onClick={handleClick} className="px-10 py-8 md:hidden md:h-fit" src={Menu} alt="" />
