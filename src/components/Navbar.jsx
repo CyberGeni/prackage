ca163a30d736318e6b5ead0cb6cb2a18f37a566a
Treasure Ajefu <treasureajefu@gmail.com>
2022-12-20 13:47:17 +0100

    fixed menu not displaying

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,11 +15,11 @@ function Navbar() {
   }
 
   return (
-    <header className="flex justify-between mx-auto list-none overflow-hidden w-full bg-transparent fixed z-10 backdrop-blur-3xl">
-        <h1 className="px-10 py-6 sm:px-14 sm:py-5 md:px-18 md:py-8 font-bold text-2xl">prackage</h1>
+    <header className={isActive ? "fixed flex justify-between mx-auto list-none overflow-hidden w-full" : "fixed flex justify-between mx-auto list-none overflow-hidden w-full backdrop-blur-3xl"}>
+        <h1 className="px-10 py-6 font-bold text-2xl">prackage</h1>
         <div className=
           {isActive ? "left-[0vw] md:left-unset transition-all flex flex-col md:flex-row justify-between w-full h-screen md:h-fit text-2xl md:text-base space-y-4 md:space-y-0 fixed md:relative bg-white" : "transition-all left-[100vw] absolute md:relative md:flex md:justify-between w-full md:left-0 md:text-lg md:my-2"}>
-          <div className="flex justify-between px-10 py-8 md:px-0 md:py-0">    
+          <div className="flex justify-between px-10 py-5 md:px-0 md:py-0">    
             <h1 className="font-bold md:hidden">prackage</h1>
             <img 
               onClick={handleClick}
@@ -27,19 +27,18 @@ function Navbar() {
               src={Close}
               />
           </div>
-          <nav className="space-y-7 md:w-full md:space-y-0 md:flex md:items-center md:justify-center px-10  md:px-0 md:space-x-4 lg:space-x-5 text-gray-900">
+          <nav className="space-y-7 md:w-full md:space-y-0 md:flex md:items-center md:justify-center px-10 md:py-3 md:px-0 md:space-x-5">
             <li>Home</li>
             <li>About us</li>
             <li>Pricing</li>
-            <li>Blog</li>
           </nav>
 
-          <div className="flex flex-col w-auto md:space-x-3 md:pr-16 md:flex-row md:items-center md:justify-center space-y-4 md:space-y-0">
+          <div className="flex flex-col w-auto md:space-x-3 md:pr-10 md:flex-row md:items-center md:justify-center space-y-4 md:space-y-0">
             <button className="md:w-max">Log In</button>
             <button className="text-xl md:text-base md:rounded  md:w-max w-full bg-blue-600 mb-0 text-white px-6 py-4 md:py-2 ">Sign Up</button>
           </div>
         </div>
-        <img onClick={handleClick} className="px-10 py-8 sm:px-14 sm:py-10  md:hidden md:h-fit" src={Menu} alt="" />
+        <img onClick={handleClick} className="px-10 py-8 md:hidden md:h-fit" src={Menu} alt="" />
     </header>
   )
 }
