cda59da6a59acce6e257229f7b1656f0b3c764c3
Treasure Ajefu <treasureajefu@gmail.com>
2022-12-28 20:07:59 +0100

    extracted carousel into component

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,117 @@
+import React from "react";
+import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+import Foza from "../assets/images/foza.png";
+import FozaMini from "../assets/images/foza-mini.png";
+import Mojirayo from "../assets/images/mojirayo.png";
+import MojirayoMini from "../assets/images/mojirayo-mini.png";
+import Toheeb from "../assets/images/toheeb.png";
+import ArrowRight from "../assets/icons/arrow-right.svg";
+import ArrowLeft from "../assets/icons/arrow-left.svg";
+
+function Carousel() {
+  const options = {
+    type: "loop",
+    perPage: 1,
+    autoplay: true,
+    pauseOnHover: false,
+    resetProgress: false,
+  };
+
+  const slides = [
+    {
+      id: 1,
+      name: "Foza Fawehinmi",
+      position: "CEO, Digital Music Commerce and Exchange",
+      image: Foza,
+      imageMini: FozaMini,
+      quote:
+        "Who is set to release her debut book, the memoir titled “So Far So Foza”, on the 17th of February, 2023.",
+    },
+    {
+      id: 2,
+      name: "Mojirayo “Amoke OnTop” Olaniyan",
+      position: "Ex-branch manager, United Bank for Africa (UBA)",
+      image: Mojirayo,
+      imageMini: MojirayoMini,
+      quote:
+        "Who retired recently after 16 meritorious years in the Nigerian banking industry.",
+    },
+    {
+      id: 3,
+      name: "Toheeb Balogun",
+      position: "Co-founder & Director of Impact, The Hook Creative Agency",
+      image: Toheeb,
+      imageMini: FozaMini,
+      quote:
+        "On the successful presentation of “Access to Blow” to His Excellency, The Lagos State Governor Babajide Sanwo-Olu.",
+    },
+  ];
+  return (
+    <div className="bg-black pb-2 sm:pb-0">
+      <Splide options={options} hasTrack={false} aria-label="...">
+        <SplideTrack>
+          {slides.map((slide) => (
+            <SplideSlide className="bg-black text-white h-fit">
+              {/* desktop carousel */}
+              <section className="hidden md:flex ">
+                <div className="sm:px-16 lg:px-24 sm:py-10 md:py-16 sm:flex sm:flex-col sm:justify-between sm:place-content-between">
+                  <div>
+                    <h1 className="md:text-4xl lg:text-5xl my-4 font-semibold">
+                      Recognizing:
+                    </h1>
+                    <h3 className="text-white sm:text-lg md:text-xl font-semibold mt-4 my-2">
+                      {slide.name}
+                    </h3>
+                    <p className="text-gray-300">{slide.position}</p>
+                  </div>
+                  <div className="mb-16">
+                    <p className="text-gray-300 my-2 text-lg lg:text-xl">
+                      {slide.quote}
+                    </p>
+                  </div>
+                </div>
+                <div className="w-full">
+                  <img
+                    className="w-full md:max-w-xl md:ml-auto"
+                    src={slide.image}
+                    alt=""
+                  />
+                </div>
+              </section>
+              {/* mobile carousel */}
+              <section className="md:hidden px-12 sm:px-16">
+                <h1 className="text-3xl mt-10 my-6 font-semibold">
+                  Recognizing:
+                </h1>
+                <img className="w-fit" src={slide.imageMini} alt="" />
+                <h3 className="text-white text-xl font-semibold mt-4 my-3">
+                  {slide.name}
+                </h3>
+                <p className="text-gray-300 text-xl">{slide.position}</p>
+                <p className="text-gray-300 my-2 text-lg">{slide.quote}</p>
+              </section>
+            </SplideSlide>
+          ))}
+        </SplideTrack>
+
+        {/* progress bar */}
+        <div className="splide__progress mt-2 mb-20 md:mb-0">
+          <div className="splide__progress__bar bg-blue-600" />
+        </div>
+
+        {/* controls */}
+        <div class="splide__arrows relative -top-12 md:-top-28 flex w-44 px-12 md:ml-6 lg:ml-12 space-x-4 my-2">
+          <button class="splide__arrow splide__arrow--prev bg-transparent mx-6 my-5 p-4 w-auto h-auto">
+            <img src={ArrowLeft} alt="" />
+          </button>
+          <button class="splide__arrow splide__arrow--next bg-transparent mx-6 my-5 p-4 w-auto h-auto">
+            <img src={ArrowRight} alt="" />
+          </button>
+        </div>
+      </Splide>
+    </div>
+  );
+}
+
+export default Carousel;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,57 +2,12 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import PeopleTouchingHands from "../assets/images/about-img.png";
-import Foza from "../assets/images/foza.png";
-import FozaMini from "../assets/images/foza-mini.png";
-import Mojirayo from "../assets/images/mojirayo.png";
-import MojirayoMini from "../assets/images/mojirayo-mini.png";
-import Toheeb from "../assets/images/toheeb.png";
-import ArrowRight from "../assets/icons/arrow-right.svg";
-import ArrowLeft from "../assets/icons/arrow-left.svg";
 import Whatsapp from "../assets/icons/whatsapp.svg";
 import Email from "../assets/icons/email.svg";
-import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
-import "@splidejs/react-splide/css";
 import "../App.css";
+import Carousel from "../components/Carousel";
 
 function About() {
-  const options = {
-    type: "loop",
-    perPage: 1,
-    autoplay: true,
-    pauseOnHover: false,
-    resetProgress: false,
-  };
-
-  const slides = [
-    {
-      id: 1,
-      name: "Foza Fawehinmi",
-      position: "CEO, Digital Music Commerce and Exchange",
-      image: Foza,
-      imageMini: FozaMini,
-      quote:
-        "Who is set to release her debut book, the memoir titled “So Far So Foza”, on the 17th of February, 2023.",
-    },
-    {
-      id: 2,
-      name: "Mojirayo “Amoke OnTop” Olaniyan",
-      position: "Ex-branch manager, United Bank for Africa (UBA)",
-      image: Mojirayo,
-      imageMini: MojirayoMini,
-      quote:
-        "Who retired recently after 16 meritorious years in the Nigerian banking industry.",
-    },
-    {
-      id: 3,
-      name: "Toheeb Balogun",
-      position: "Co-founder & Director of Impact, The Hook Creative Agency",
-      image: Toheeb,
-      imageMini: FozaMini,
-      quote:
-        "On the successful presentation of “Access to Blow” to His Excellency, The Lagos State Governor Babajide Sanwo-Olu.",
-    },
-  ];
   return (
     <div className="font-BrOmega">
       <Navbar />
@@ -78,6 +33,7 @@ function About() {
           </div>
         </section>
         <img src={PeopleTouchingHands} alt="" />
+        
         {/* our story */}
         <section className="px-12 py-16 sm:px-16 md:px-24 md:py-10 my-4 grid md:grid-cols-2 gap-x-12">
           <h4 className="text-xl md:text-2xl my-3 font-semibold md:col-span-2">
@@ -114,96 +70,10 @@ function About() {
             </p>
           </div>
         </section>
+        
         {/* carousel goes here */}
-        <div className="bg-black pb-2 sm:pb-0">
-          <Splide options={options} hasTrack={false} aria-label="...">
-            <SplideTrack>
-              {slides.map((slide) => (
-                <SplideSlide className="bg-black text-white h-fit">
-                  <section className="hidden md:flex ">
-                    <div className="sm:px-16 lg:px-24 sm:py-10 md:py-16 sm:flex sm:flex-col sm:justify-between sm:place-content-between">
-                      <div>
-                        <h1 className="md:text-4xl lg:text-5xl my-4 font-semibold">
-                          Recognizing:
-                        </h1>
-                        <h3 className="text-white sm:text-lg md:text-xl font-semibold mt-4 my-2">
-                          {slide.name}
-                        </h3>
-                        <p className="text-gray-300">{slide.position}</p>
-                      </div>
-                      <div className="mb-16">
-                        <p className="text-gray-300 my-2 text-lg lg:text-xl">{slide.quote}</p>
-                      </div>
-                    </div>
-                    <div className="w-full">
-                      <img
-                        className="w-full md:max-w-xl md:ml-auto"
-                        src={slide.image}
-                        alt=""
-                      />
-                    </div>
-                  </section>
-                  {/* <div className="sm:px-16 lg:px-24 sm:py-10 md:py-16 sm:flex sm:flex-col sm:justify-between sm:place-content-between">
-                    <h1 className="sm:text-3xl md:text-4xl lg:text-5xl my-4 font-semibold">
-                      Recognizing:
-                    </h1>
-                    <div>
-                      <p className="text-gray-300 my-2">{slide.quote}</p>
-                    </div>
-                    <div className="md:mb-28">
-                      <h3 className="text-white sm:text-lg md:text-xl font-semibold mt-4 my-2">
-                        {slide.name}
-                      </h3>
-                      <p className="text-gray-300">{slide.position}</p>
-                    </div>
-                  </div>
-                  <div className="order-first md:order-last w-full my-4 sm:my-0">
-                    <img
-                      className="hidden md:block order-first w-full md:max-w-xl md:ml-auto"
-                      src={slide.image}
-                      alt=""
-                    />
-                    <img
-                      className="md:hidden block order-first w-full"
-                      src={slide.imageMini}
-                      alt=""
-                    />
-                  </div> */}
-                  {/* mobile carousel */}
-                  <section className="md:hidden px-12 sm:px-16">
-                    <h1 className="text-3xl mt-10 my-6 font-semibold">
-                      Recognizing:
-                    </h1>
-                    <img className="w-fit" src={slide.imageMini} alt="" />
-                    <h3 className="text-white text-xl font-semibold mt-4 my-3">
-                      {slide.name}
-                    </h3>
-                    <p className="text-gray-300 text-xl">{slide.position}</p>
-                    <p className="text-gray-300 my-2 text-lg">{slide.quote}</p>
-                  </section>
-                </SplideSlide>
-              ))}
-            </SplideTrack>
-
-            <div className="splide__progress mt-2 mb-20 md:mb-0">
-              <div className="splide__progress__bar bg-blue-600" />
-            </div>
-
-            <div class="splide__arrows relative -top-12 md:-top-28 flex w-44 px-12 md:ml-6 lg:ml-12 space-x-4 my-2">
-              <button class="splide__arrow splide__arrow--prev bg-transparent mx-6 my-5 p-4 w-auto h-auto">
-                <img src={ArrowLeft} alt="" />
-              </button>
-              <button class="splide__arrow splide__arrow--next bg-transparent mx-6 my-5 p-4 w-auto h-auto">
-                <img src={ArrowRight} alt="" />
-              </button>
-            </div>
-            {/* <button class="splide__toggle" type="button">
-              <span class="splide__toggle__play">Play</span>
-              <span class="splide__toggle__pause">Pause</span>
-            </button> */}
-          </Splide>
-        </div>
-
+        <Carousel />
+        
         {/* contact */}
         <section className="px-10 py-6 sm:px-14 md:px-16 md:py-10 my-2">
           <h2 className="text-gray-900 text-xl md-text-2xl font-semibold my-2">
