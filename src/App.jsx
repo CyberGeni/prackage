b198bffa5647ff69207a4ac05be81bd55c605d8c
Treasure Ajefu <treasureajefu@gmail.com>
2023-01-11 23:43:40 +0100

    fix: scroll restoration when switching routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
-import './App.css'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Humark from './pages/Humark'
-import Pricing from './pages/Pricing'
-
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Humark from "./pages/Humark";
+import Pricing from "./pages/Pricing";
+import ScrollToTop from "./components/ScrollToTop";
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/humark" element={<Humark />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="*" element={<div className='flex items-center justify-center h-screen text-6xl font-semibold font-BrOmega bg-gray-800 text-gray-200'>404</div>} />
-        </Routes>
+        <ScrollToTop>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/humark" element={<Humark />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route
+              path="*"
+              element={
+                <div className="flex items-center justify-center h-screen text-6xl font-semibold font-BrOmega bg-gray-800 text-gray-200">
+                  404
+                </div>
+              }
+            />
+          </Routes>
+        </ScrollToTop>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
